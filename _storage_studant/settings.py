5f11679efbae72833d8930072c43efd05fe1647a
Jones Torres <jonestorresadv@icloud.com>
2023-10-29 15:34:19 -0300

    fix: staticfiles

--- _storage_studant/settings.py
+++ _storage_studant/settings.py
@@ -114,7 +114,7 @@ if DATABASES_URL:
     DATABASES["default"].update(db_from_env)
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
